0ed841f6c876db28af7b1fb58739b4ba1f69fec9
Polina Skvortsova <poliskvor@gmail.com>
2022-10-13 20:16:49 +0300

    Initial commit

---
+++ src/Mine.java
@@ -0,0 +1,11 @@
+public class Mine {
+    public static void main(String[] args) {
+        int ticketPrice = 12_000; // стоимость билета
+        int roublesForOneMiles = 20; // стоимость одной мили
+
+
+        System.out.println("Количество миль ");
+        System.out.println(ticketPrice / roublesForOneMiles + " миль");
+
+    }
+}
